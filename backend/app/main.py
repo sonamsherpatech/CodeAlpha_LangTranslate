1868f2afbca5571b7f31010eab40ab68d5d7fb2a
sonamsherpatech <sonam.sherpa.tech@gmail.com>
2026-09-04 13:37:12 +0545

    fix: allow CORS from all origins for vercel deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,14 +8,14 @@ app = FastAPI(
     version="1.0.0"
 )
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# origins = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
